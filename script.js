1cca2c805fed19c54caec013f76c6605550543c6
Shawn McPhee <scmcphee9@gmail.com>
2020-10-31 18:24:40 -0600

    made variables to pull for current weather

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var key = 0;
+var recentSearch =
+  JSON.parse(window.localStorage.getItem("recentSearch")) || [];
+var ulEl = $("#recentSearch");
+var cardEl = $("#currentWeather");
+
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
 
@@ -11,15 +17,27 @@ $("#searchBtn").on("click", function (event) {
 
   // local storage
 
-  var recentSearch = [];
-  recentSearch.push(city);
-  localStorage.setItem("cityRequest", JSON.stringify(recentSearch));
+  recentSearch.unshift(city);
+  function localStorageSave() {
+    localStorage.setItem(key, JSON.stringify(recentSearch));
+  }
+  localStorageSave();
+  //this does not appear to be pushing to recent search array
+
   console.log(recentSearch);
 
-  for (var i = 0; i < recentSearch.length; i++) {
-    var savedSearch = localStorage.getItem(recentSearch[i]);
+  function makeHistory(city) {
+    console.log(city);
+    var liEl = $("<li>").addClass("cityList").text(city);
+    $("#recentSearch").append(liEl);
   }
-  $("#recentSearch").append(savedSearch);
+
+  for (var i = 0; i < recentSearch.length; i++) {
+    var savedSearch = recentSearch[i];
+    console.log(savedSearch);
+
+    makeHistory(savedSearch);
+  }
 
   // variables for API links
   var currentWeather =
@@ -34,46 +52,62 @@ $("#searchBtn").on("click", function (event) {
 
   // fetch requests on weather APIs
 
-  // fetch(currentWeather)
-  //   .then(function (response) {
-  //     return response.json();
-  //   })
-  //   .then(function (data) {
-  //     console.log("current -------------------");
-  //     console.log(data);
+  fetch(currentWeather)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("current -------------------");
+      //console.log(data);
 
-  //     // defines lat and lon coordinates of searched city
-  //     var lat = data.coord.lat;
-  //     var lon = data.coord.lon;
+      // defines lat and lon coordinates of searched city
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      //var currentDate =
+      var currentCity = data.name + "(date)";
+      var currentTemp = data.main.temp + " °F";
+      var currentHumid = data.main.humidity + "%";
+      var currentWind = data.wind.speed + " mph";
+      console.log(currentCity);
+      console.log(currentTemp);
+      console.log(currentHumid);
+      console.log(currentWind);
 
-  //     console.log(lat);
-  //     console.log(lon);
+      var cityTitle = $("<h4>").text(currentCity);
+      $("#currentWeather").append(cityTitle);
 
-  //     // defines UV index api url
-  //     var uvIndex =
-  //       "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-  //       lat +
-  //       "&lon=" +
-  //       lon +
-  //       "&appid=d7ca7edce9a0915ba4502c508b220e07";
+      // console.log(lat);
+      // console.log(lon);
 
-  //     // fetch request for uv index
-  //     fetch(uvIndex)
-  //       .then(function (response) {
-  //         return response.json();
-  //       })
-  //       .then(function (data) {
-  //         console.log("UV Index ------------------");
-  //         console.log(data);
-  //       });
-  //   });
+      // defines UV index api url
+      var uvIndex =
+        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=d7ca7edce9a0915ba4502c508b220e07";
 
-  // fetch(fiveDay)
-  //   .then(function (response) {
-  //     return response.json();
-  //   })
-  //   .then(function (data) {
-  //     console.log("five Day -----------------");
-  //     console.log(data);
-  //   });
+      // fetch request for uv index
+      fetch(uvIndex)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log("UV Index ------------------");
+          // console.log(data);
+          var currentUVIndex = data.value;
+          console.log(currentUVIndex);
+        });
+    });
+
+  fetch(fiveDay)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("five Day -----------------");
+      console.log(data);
+
+      // for loop to pull each object for future days
+    });
 });
